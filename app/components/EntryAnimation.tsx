fcd646e77b268818605764135f9e02200e9e5a83
RIFX Marketing <rifxmarketing@gmail.com>
2026-05-06 01:19:25 -0500

    perf: optimize intro animations for desktop fluidness

--- app/components/EntryAnimation.tsx
+++ app/components/EntryAnimation.tsx
@@ -28,6 +28,8 @@ export default function EntryAnimation() {
             animation: `particle-float ${p.duration}s ease-out ${p.delay}s forwards`,
             left: '50%',
             top: '50%',
+            willChange: 'transform, opacity',
+            backfaceVisibility: 'hidden'
           } as React.CSSProperties}
         />
       ))}
@@ -37,26 +39,27 @@ export default function EntryAnimation() {
         src="/images/mars-user.png"
         alt="Mars Planet"
         className="absolute w-48 h-48 md:w-80 md:h-80 object-contain animate-flare-erupt opacity-0 blur-[2px] z-10"
+        style={{ backfaceVisibility: 'hidden', willChange: 'transform, opacity' }}
       />
 
       <div className="relative flex flex-col items-center z-20">
         {/* Flying Brand Reveal Assembled */}
         <div className="relative flex flex-col items-center px-4 w-64 h-64 md:w-[28rem] md:h-[28rem]">
           {/* Letra R */}
-          <img src="/piezas-logo/letra-r.png" alt="R" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.2s' }} />
+          <img src="/piezas-logo/letra-r.png" alt="R" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.2s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Letra I */}
-          <img src="/piezas-logo/letra-i.png" alt="I" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.4s' }} />
+          <img src="/piezas-logo/letra-i.png" alt="I" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.4s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Letra F */}
-          <img src="/piezas-logo/letra-f.png" alt="F" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.6s' }} />
+          <img src="/piezas-logo/letra-f.png" alt="F" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.6s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Letra X */}
-          <img src="/piezas-logo/letra-x.png" alt="X" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.8s' }} />
+          <img src="/piezas-logo/letra-x.png" alt="X" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '0.8s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Guion - */}
-          <img src="/piezas-logo/letra-.png" alt="-" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.0s' }} />
+          <img src="/piezas-logo/letra-.png" alt="-" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.0s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Texto Marketing */}
-          <img src="/piezas-logo/texto-marketing.png" alt="Marketing" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.2s' }} />
+          <img src="/piezas-logo/texto-marketing.png" alt="Marketing" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.2s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
           {/* Astronauta y Corona aterrizando */}
-          <img src="/piezas-logo/corona.png" alt="Corona" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.6s' }} />
-          <img src="/piezas-logo/astronauta.png" alt="Astronauta" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.6s' }} />
+          <img src="/piezas-logo/corona.png" alt="Corona" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.6s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
+          <img src="/piezas-logo/astronauta.png" alt="Astronauta" className="absolute inset-0 w-full h-full object-contain opacity-0 animate-logo-fly" style={{ animationDelay: '1.6s', backfaceVisibility: 'hidden', willChange: 'transform, opacity' }} />
         </div>
 
         {/* Ambient Residual Glow */}
