eb1831c6da134cd8e3a72b42b695486c0212f9ec
RIFX Marketing <rifxmarketing@gmail.com>
2026-06-17 18:19:05 -0500

    fix: Remove hardcoded favicon to allow icon.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,11 +47,7 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
+
 };
 
 export const viewport = {
